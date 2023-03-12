756c5acab367c4ff3ba6a34fda6b18c0715f4ae5
minghangyou <33722903+minghangyou@users.noreply.github.com>
2023-03-12 12:17:45 +0800

    Update AdaptiveThreadPool.h

--- AdaptiveThreadPool.h
+++ AdaptiveThreadPool.h
@@ -17,8 +17,8 @@ class AdaptiveThreadPool {
 public:
     AdaptiveThreadPool(int minSize, int maxSize, int idleTime)
         : minSize_(minSize), maxSize_(maxSize), idleTime_(idleTime),
-        taskQueue_(), mutex_(), notEmpty_(), notFull_(),
-        threads_size_(minSize),taskCount_(0), running_(false), idleCount_(0)
+        taskQueue_(), mutex_(), queue_mutex_(), notEmpty_(), notFull_(),
+        threads_size_(minSize),taskCount_(0), running_(false), idleCount_(0),stop_flag_(0)
     {
         running_ = true;
         for (int i = 0; i < minSize_; ++i) {
@@ -43,7 +43,7 @@ public:
     auto submit(F&& task) 
     {
         {
-            std::lock_guard<std::mutex> lock(mutex_);
+            std::lock_guard<std::mutex> lock(queue_mutex_);
             taskQueue_.emplace(std::forward<F>(task));
             ++taskCount_;
         }
@@ -55,7 +55,7 @@ private:
     inline void startThread() {
         std::thread([this]()
         {
-            std::unique_lock<std::mutex> locker(mutex_);
+            std::unique_lock<std::mutex> locker(queue_mutex_);
             while(true){
                 if(taskCount_>0){
                     auto task = std::move(taskQueue_.front());
@@ -74,29 +74,8 @@ private:
                 else notEmpty_.wait(locker);
             }}).detach();
     }
-    void threadFunc() {
-        std::unique_lock<std::mutex> locker(mutex_);
-        while(true){
-            if(taskCount_>0){
-                auto task = std::move(taskQueue_.front());
-                taskQueue_.pop();
-                --taskCount_;
-                locker.unlock();
-                if (task) {
-                    task();
-                }
-                else {
-                    break;
-                }
-                locker.lock();
-            }
-            else if(!running_) break;
-            else notEmpty_.wait(locker);
-        }
-    }
 
     void adjustPoolSize() {
-        std::unique_lock<std::mutex> lock(mutex_);
         double taskEfficiency = static_cast<double>(taskCount_) / static_cast<double>(threads_size_);
         if (taskEfficiency >2.0 && threads_size_ < maxSize_) {
             int newThreads = std::min(static_cast<int>(std::ceil(threads_size_ * (taskEfficiency - 1.0))), static_cast<int>(maxSize_ - threads_size_));
@@ -105,12 +84,24 @@ private:
                 ++threads_size_;
             }
         }
-        else if (taskEfficiency < 0.5 && threads_size_ > minSize_) {
-            int threadsToStop = std::min(static_cast<int>(std::ceil(threads_size_ * (1.0 - taskEfficiency) / 2)), threads_size_ - minSize_);// 每次将空闲进程减半
-            while (threadsToStop > 0) {
-                taskQueue_.emplace(nullptr);
-                --threadsToStop;
-                --threads_size_;
+        else if (taskEfficiency < 1.0 && threads_size_ > minSize_) {
+            ++stop_flag_;
+            if(stop_flag_>=3){
+                stop_flag_ = 0;
+                int threadsToStop = std::min(static_cast<int>(std::ceil(threads_size_ * (1.0 - taskEfficiency) / 2)), threads_size_ - minSize_);// 每次将空闲进程减半
+                
+                LOG_ERROR("threads_size_:%d,threadsToStop:%d/n",static_cast<int>(threads_size_),threadsToStop); 
+                {   
+                    std::lock_guard<std::mutex> lock(queue_mutex_);
+                    while (threadsToStop > 0) {
+                        taskQueue_.emplace(nullptr);
+                        --threads_size_;
+                        ++taskCount_;
+                        --threadsToStop;
+                        notEmpty_.notify_one();
+                    }
+                }
+                
             }
         }
     }
@@ -121,12 +112,14 @@ private:
     int idleTime_;
     std::queue<std::function<void()>> taskQueue_;
     std::mutex mutex_;
+    std::mutex queue_mutex_;
     std::condition_variable notEmpty_;
     std::condition_variable notFull_;
     std::atomic<int> threads_size_;
     std::atomic<int> taskCount_;
     std::atomic<bool> running_;
     int idleCount_;
+    int stop_flag_;
 
 };
 
